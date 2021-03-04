8b36fa32c67d84823a1cea43da3ff9670a81c788
Jordan Carter <jordan.carter.0023@gmail.com>
2021-03-04 15:35:08 -0600

    started classes exercise

--- Program.cs
+++ Program.cs
@@ -1,12 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace classes
 {
+    public class Company
+    {
+
+        // Some readonly properties (let's talk about gets, baby)
+
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        // Create a public property for holding a list of current employees
+        public List<string> EmployeesList { get; set; }
+        public EmployeesList(List<string> Employees)
+        {
+            employeesList = Employees;
+        }
+
+        /*
+        Create a constructor method that accepts two arguments:
+         1. The name of the company
+         2. The date it was created
+
+        The constructor will set the value of the public properties
+        */
+        public Company(string name, DateTime createdOn)
+        {
+            this.Name = name;
+            this.CreatedOn = createdOn;
+
+        }
+
+    }
+
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string title { get; set; }
+        public DateTime startDate { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an instance of a company. Name it whatever you like.
+            Company seeSharp = new Company("SeeSharp", new DateTime());
+
+            // Create three employees
+            Employee jimmy = new Employee()
+            {
+                firstName = "Jimmy",
+                lastName = "John",
+                title = "Destroyer of Galaxies",
+                startDate = new DateTime()
+            };
+            Employee john = new Employee()
+            {
+                firstName = "John",
+                lastName = "Jimmy",
+                title = "Builder of Galaxies",
+                startDate = new DateTime()
+            };
+            Employee jimbo = new Employee()
+            {
+                firstName = "Jimbo",
+                lastName = "Johnsmith",
+                title = "Destroyer of Toilets",
+                startDate = new DateTime()
+            };
+
+            // Assign the employees to the company
+            List<string> Employees = new List<string>();
+            Employees.Add(jimmy);
+            Employees.Add(john);
+            Employees.Add(jimbo);
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
         }
     }
 }
